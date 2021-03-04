f4d930c1a9b0cbfd5af5d5dcd7a26fe1e6a87910
Duaa momani <79081020+duaa-almomani99@users.noreply.github.com>
2021-03-04 20:39:11 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,73 @@
+'use strict';
+let myName='duaa ';
+let myNationality='jordainian';
+let myfavoriteAnimal="hippo";
+let myfavoriteColor="green";
+let myLength='164'
+let myfavMovie='up'
+let userScore=0;
+
+
+let userName=prompt('welcome to you on my website.what is your name?')
+alert('hi'+ userName + 'answer with yes or no ');
+
+let name =prompt(' my name is duaa ');
+if (name.toloercase() === no || name.toloercase()==='n'){
+    alert('thats true , my names duaa');
+userScore+=1;
+}else {
+    alert('thats fales, my name is duaa')
+} 
+
+
+
+
+let nationality=prompt(' my nationality is jordanian  ');
+if (nationality.toloercase() === 'yes' || nationality.toloercase()==='y'){
+    alert('thats true ');
+userScore+=1;
+}else {
+    alert('thats fales, my nationality is jordanian');
+
+
+
+} 
+
+
+
+let favoriteAnimal =prompt(' my favorite animal is hippo');
+if (name.toloercase() === 'yes' || name.toloercase()==='y'){
+    alert('thats true ,my favorite animal is hippo' );
+userScore+=1;
+}else {
+    alert('thats fales, my favorite animal is hippo')
+
+
+} 
+
+
+
+let favcolor =prompt(' my favorite color is green ');
+if (favcolor.toloercase() === 'yes' || favcolor.toloercase()==='y'){
+    alert('thats true ,my favorite color is green' );
+userScore+=1;
+}else {
+    alert('thats fales, my favorite color is green ')
+
+    
+
+} 
+
+
+
+
+let movie =prompt(' my favorite movie is up');
+if (movie.toloercase() === 'yes' || movie.toloercase()==='y'){
+    alert('thats true ,my favorite movie is up' );
+userScore+=1;
+}else {
+    alert('thats fales, my favorite movie is up')
+
+}
+
+
